5295438d830b5ec483655b4c2cf2be5853b4da75
Shawncgy <shawn.scgy@gmail.com>
2024-06-29 23:31:09 -0600

    finish a new version for flight page, add classes the instructor recommended. Add a little change for reserve page.

---
+++ Data/FlightService.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment2.Data
+{
+    public class FlightService
+    {
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..//..//..//..//..//Resources//Res//airports.csv");
+        public static List<string> airports = new List<string>();
+
+        public FlightService()
+        {
+            populateAirports();
+        }
+        private void populateAirports()
+        {
+            string airport;
+            foreach (string line in File.ReadAllLines(filePath))
+            {
+                airports.Add(line);
+            }
+        }
+        public static List<string> GetAirports()
+        {
+            return airports;
+        }
+    }
+}
